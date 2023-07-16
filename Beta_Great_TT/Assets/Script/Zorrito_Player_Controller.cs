47708b697ef4c39ff6eb20b7d5e587bc12eb386a
Eiinner <86447351+Chutsa5776@users.noreply.github.com>
2023-07-16 13:05:28 -0600

    Beta_Great_TT

--- Beta_Great_TT/Assets/Script/Zorrito_Player_Controller.cs
+++ Beta_Great_TT/Assets/Script/Zorrito_Player_Controller.cs
@@ -7,8 +7,7 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-       // Application.targetFrameRate = 60;
+        
     }
 
     // Update is called once per frame
