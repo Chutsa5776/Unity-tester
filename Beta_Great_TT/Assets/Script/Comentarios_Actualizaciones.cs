8fa1eaec98d6d5a26dd43636ce14a0f80f8c8322
Eiinner <86447351+Chutsa5776@users.noreply.github.com>
2023-07-19 00:20:09 -0600

    Beta

--- Beta_Great_TT/Assets/Script/Comentarios_Actualizaciones.cs
+++ Beta_Great_TT/Assets/Script/Comentarios_Actualizaciones.cs
@@ -1,26 +1,2 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Comentarios_Actualizaciones : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 // yo agrege un circulo nuevo pero borre el zorry
 
-
-
-
-
-}
